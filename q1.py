8f1d684e22e1cf95d6f082f605c03281100f598f
wilsindev <jose.neto1074@aluno.ce.gov.br>
2025-09-02 10:44:22 -0300

    Commit avaliação prática – Questões resolvidas

--- q1.py
+++ q1.py
@@ -3,5 +3,9 @@ if nm % 2 == 0 and nm < 0:
     print("Número é par e negativo")
 elif nm % 2 == 0 and nm > 0:
     print("Número é par e positivo")
+elif nm % 2 != 0 and nm > 0:
+    print("Número é impar e positivo")
+elif nm % 2 != 0 and nm < 0:
+    print("Número é impar e negativo")
 if nm == 0:
     print("Número é zero")
